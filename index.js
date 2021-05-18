7ee884474df777e8458d499d2d7862bcf014c352
Hamna Kamran <74135890+hamnakamran10@users.noreply.github.com>
2021-05-18 08:46:00 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+//This command use to print on
+//Google
+console.log("welcome to js")
+//variable
+
+//var name = "hamna";
+//console.log(name);
+
+//var age = 15;
+//console.log(age)
+
+var firstname = 'Hamna';
+var lastname = 'Kamran';
+var space=' '; //for creating space
+var age = 15;
+
+
+
+ console.log(firstname+space + lastname)//for proffesional
+ console.log(firstname +' '+ lastname +space+ 'My Age is =' + age)//for begginers
+
+ var age =  prompt('Let us know your name?')
+ document.getElementById('myname').innerHTML=age;
+
+var age =  prompt('What is your age?')
+ document.getElementById('myage').innerHTML=age;
+
+var age = prompt('What is your hobbies?')
+document.getElementById('myhobby').innerHTML=age;
+
+var age = prompt('Are u a Web Develper?')
+document.getElementById('myweb').innerHTML=age;
+
+var age = prompt('Your Educational Institute?')
+document.getElementById('myscl').innerHTML=age;
+
+
+ 
+ 
+
